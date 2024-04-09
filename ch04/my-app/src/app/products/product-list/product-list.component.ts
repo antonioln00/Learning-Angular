ba86435c6beb453cd087d117e998c95a527cc180
antonioln00 <antonioln00@gmail.com>
2024-04-09 09:29:24 -0300

    feat: ch04 part final

--- ch04/my-app/src/app/products/product-list/product-list.component.ts
+++ ch04/my-app/src/app/products/product-list/product-list.component.ts
@@ -1,17 +1,25 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { ProductDetailComponent } from '../product-detail/product-detail.component';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent {
+export class ProductListComponent implements AfterViewInit {
+  @ViewChild(ProductDetailComponent) productDetail:
+    | ProductDetailComponent
+    | undefined;
 
   selectedProduct = 'Microphone';
 
+  ngAfterViewInit(): void {
+    if (this.productDetail) {
+      console.log(`after view init${this.productDetail.name}`);
+    }
+  }
+
   onBuy(name: string) {
     window.alert(`You just bought ${name}!`);
-   }
-
-
+  }
 }
