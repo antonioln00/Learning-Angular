d211b2d1eff7a49e53d3a8f8c637b56903d5d6d0
antonioln00 <antonioln00@gmail.com>
2024-05-03 09:56:17 -0300

    feat: ch11 part4

--- ch11/src/app/products/product-detail/product-detail.component.ts
+++ ch11/src/app/products/product-detail/product-detail.component.ts
@@ -45,7 +45,9 @@ export class ProductDetailComponent implements OnInit, OnChanges {
   }
 
   changePrice(product: Product) {
-    this.dialog.open(PriceComponent).afterClosed().pipe(
+    this.dialog.open(PriceComponent, {
+      data: product.price
+    }).afterClosed().pipe(
       filter(price => !!price),
       switchMap(price => this.productService.updateProduct(product.id,
         price))
